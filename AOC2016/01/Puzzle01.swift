1795f661aeacfbe8ebb6cbac4ca0d74c35ea3cd3
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2021-09-02 15:09:07 +0530

    Solutions for 01

--- AOC2016/01/Puzzle01.swift
+++ AOC2016/01/Puzzle01.swift
@@ -5,13 +5,48 @@
 //  Created by Jaydeep Joshi on 02/09/21.
 //
 
+import ComplexModule
+
+fileprivate typealias Instruction = (turn: Character, steps: Int)
+
 struct Puzzle01: Puzzle {
+    private let instructions: [Instruction]
+
+    init() {
+        instructions = InputFileReader.read("Input01")[0].components(separatedBy: ", ").map { str in
+            let turn = str.first!
+            let steps = Int(str.dropFirst())!
+            return (turn, steps)
+        }
+    }
 
     func part1() {
-        
+        var direction = Complex(0, 1)
+        var position = Complex(0, 0)
+        instructions.forEach { instruction in
+            direction *= (instruction.turn == "L") ? Complex(0, 1) : Complex(0, -1)
+            let steps = instruction.steps
+            position += (Complex(0, Double(steps)) * direction)
+        }
+        print(position.real.magnitude + position.imaginary.magnitude)
     }
 
     func part2() {
-
+        var direction = Complex(0, 1)
+        var position = Complex(0, 0)
+        var visited: Set<Complex<Double>> = [position]
+        for instruction in instructions {
+            direction *= (instruction.turn == "L") ? Complex(0, 1) : Complex(0, -1)
+            let steps = instruction.steps
+            for _ in 1...steps {
+                position += direction
+                if visited.contains(position) {
+                    print(position.real.magnitude + position.imaginary.magnitude)
+                    return
+                } else {
+                    visited.insert(position)
+                }
+            }
+        }
     }
 }
