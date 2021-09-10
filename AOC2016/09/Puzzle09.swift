db4a707df1af99ae191a732b316564e8571bc0f9
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2021-09-10 22:20:19 +0530

    Solution for 09_01
    P2 solution gives wrong answer.
    Can't figure out why

--- AOC2016/09/Puzzle09.swift
+++ AOC2016/09/Puzzle09.swift
@@ -6,12 +6,39 @@
 //
 
 struct Puzzle09: Puzzle {
+    private let input = InputFileReader.read("Input09")[0]
 
     func part1() {
-        
+        print(length(input))
     }
 
     func part2() {
+        print(length(input, recurse: true))
+    }
 
+    func length(_ str: String, recurse: Bool = false) -> Int {
+        let chars = Array(str)
+        var count = 0
+        var index = 0
+        while index < chars.count {
+            let char = chars[index]
+            if char == "(" {
+                let endIndex = chars[(index+1)...].firstIndex(of: ")")!
+                let marker = String(chars[(index+1)..<endIndex])
+                let split = marker.split(separator: "x")
+                let len = Int(split[0])!
+                let rpt = Int(split[1])!
+                if recurse {
+                    count += (length(String(chars[(endIndex+1)...(endIndex+len)])) * rpt)
+                } else {
+                    count += (len * rpt)
+                }
+                index = endIndex + 1 + len
+            } else {
+                count += 1
+                index += 1
+            }
+        }
+        return count
     }
 }
