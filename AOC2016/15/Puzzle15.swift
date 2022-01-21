ad3dbb8f9ccb8ec75a518285897f7bc55906c537
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2022-01-21 20:28:10 +0530

    Solutions for 15

--- AOC2016/15/Puzzle15.swift
+++ AOC2016/15/Puzzle15.swift
@@ -8,10 +8,33 @@
 struct Puzzle15: Puzzle {
 
     func part1() {
-        
+        for i in 0... {
+            if isValidTime(i, p2: false) {
+                print(i)
+                break
+            }
+        }
     }
 
     func part2() {
+        for i in 0... {
+            if isValidTime(i, p2: true) {
+                print(i)
+                break
+            }
+        }
+    }
 
+    private func isValidTime(_ time: Int, p2: Bool) -> Bool {
+        var answer = (time+1) % 7 == 0 &&
+                     (time+2) % 13 == 0 &&
+                     (time+3) % 3 == 1 &&
+                     (time+4) % 5 == 3 &&
+                     (time+5) % 17 == 0 &&
+                     (time+6) % 19 == 12
+        if p2 {
+            answer = answer && (time+7) % 11 == 0
+        }
+        return answer
     }
 }
