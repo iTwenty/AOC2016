59aa98e9b54afcfd4fcdfa86e6db6d321d19bc37
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2022-01-27 01:28:17 +0530

    Solutions for 20

--- AOC2016/20/Puzzle20.swift
+++ AOC2016/20/Puzzle20.swift
@@ -5,13 +5,27 @@
 //  Created by Jaydeep Joshi on 02/09/21.
 //
 
+import Foundation
+import SE0270_RangeSet
+
 struct Puzzle20: Puzzle {
+    private var blacklist = RangeSet<Int>()
+
+    init() {
+        InputFileReader.read("Input20").forEach({ line in
+            let split = line.split(separator: "-")
+            blacklist.insert(contentsOf: Int(split[0])!..<Int(split[1])!+1)
+        })
+    }
 
     func part1() {
-        
+        var all = RangeSet(0..<4294967296)
+        all.subtract(blacklist)
+        // p1
+        print(all.ranges.first?.lowerBound ?? -1)
+        // p2
+        print(all.ranges.map(\.count).reduce(0, +))
     }
 
-    func part2() {
-
-    }
+    func part2() { }
 }
